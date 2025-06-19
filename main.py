7cebfa824dcbbf84dd538409ed37989f2fadf889
Donald Ray <syntaxray@gmail.com>
2025-06-19 10:45:58 +0100

    Add constants module

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as py
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS 
 
 
 def main():
     print('Starting Asteroids!')
-
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == '__main__':
     main()

47a54e2d0e63968ac1952e0a23e071b7823ce85a
Donald Ray <syntaxray@gmail.com>
2025-06-19 11:03:05 +0100

    Add game loop

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    py.init()
+    screen = py.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                return
+        py.Surface.fill(screen, color='black')
+        py.display.flip()
 
 if __name__ == '__main__':
     main()
